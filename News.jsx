25e67f938f9f186bd2efaaff6f42cb7125f05faa
Sinsata <103814578+Sinsata@users.noreply.github.com>
2022-10-09 21:48:46 +0500

    Update News.jsx

--- News.jsx
+++ News.jsx
@@ -1,5 +1,114 @@
-{
-  "news": [
-    
-  ]
-}
+[
+  {
+    "id": "0",
+    "image": "https://cdn.mknc.ru/contents/albums/preview/2000x2000/10000/10501/preview.jpg",
+    "title": "Birinchi yangilik",
+    "body": "Bu yangilik test tariqasida tizimga qo'shildi, Bu yangilik test tariqasida tizimga qo'shildi, Bu yangilik test tariqasida tizimga qo'shildi",
+    "type": [
+      {
+        "tibbiyot_rivoji": false,
+        "tibbiy_talim": true,
+        "emlash": false
+      }
+    ],
+    "creator": "Hojiboy Raupov",
+    "date": "2022-10-06",
+    "time": "01:12:00"
+  },
+  {
+    "id": "1",
+    "image": "https://pbs.twimg.com/media/C-bpClIXoAAX1i-.jpg",
+    "title": "Ikkinchi yangilik",
+    "body": "Bu yangilik test tariqasida tizimga qo'shildi",
+    "type": [
+      {
+        "tibbiyot_rivoji": true,
+        "tibbiy_talim": false,
+        "emlash": false
+      }
+    ],
+    "creator": "Hojiboy Raupov",
+    "date": "2022-10-06",
+    "time": "01:12:00"
+  },
+  {
+    "id": "2",
+    "image": "http://ddg.by/wp-content/uploads/2018/09/pediatr-e1537861057161-300x202.jpg",
+    "title": "Uchinchi yangilik",
+    "body": "Dasturni testlab ko'rish uchun yana yangilik qo'shib ko'rildi",
+    "type": [
+      {
+        "tibbiyot_rivoji": true,
+        "tibbiy_talim": true,
+        "emlash": false
+      }
+    ],
+    "creator": "Hojiboy Raupov",
+    "date": "2022-10-06",
+    "time": "01:20:00"
+  },
+  {
+    "id": "3",
+    "image": "https://d2r9nfiii89r0l.cloudfront.net/article/images/415x415/dimg/himss-omb-image-100709616-orig.jpg",
+    "title": "To'rtinchi yangilik",
+    "body": "Dasturni testlab ko'rish uchun yana yangilik qo'shib ko'rildi",
+    "type": [
+      {
+        "tibbiyot_rivoji": false,
+        "tibbiy_talim": true,
+        "emlash": false
+      }
+    ],
+    "creator": "Hojiboy Raupov",
+    "date": "2022-10-06",
+    "time": "01:20:00"
+  },
+  {
+    "id": "4",
+    "image": "https://1.bp.blogspot.com/-W1-i_gyYYvA/XZhqUcXDYyI/AAAAAAAAAFM/6uE_LmZ02A8nLilaOz-Jj5aiCbOA1KtVACLcBGAsYHQ/s400/Cardiology%2BHospital%2Bin%2BAjman.jpg",
+    "title": "Beshinchi yangilik",
+    "body": "Dasturni testlab ko'rish uchun yana yangilik qo'shib ko'rildi",
+    "type": [
+      {
+        "tibbiyot_rivoji": false,
+        "tibbiy_talim": false,
+        "emlash": true
+      }
+    ],
+    "creator": "Hojiboy Raupov",
+    "date": "2022-10-06",
+    "time": "01:20:00"
+  },
+  {
+    "id": "5",
+    "image": "https://www.calend.ru/img/content/i2/2850.jpg",
+    "title": "Oltinchi yangilik",
+    "body": "Dasturni testlab ko'rish uchun yana yangilik qo'shib ko'rildi",
+    "type": [
+      {
+        "tibbiyot_rivoji": true,
+        "tibbiy_talim": false,
+        "emlash": true
+      }
+    ],
+    "creator": "Hojiboy Raupov",
+    "date": "2022-10-06",
+    "time": "01:20:00"
+  },
+  {
+    "id": "6",
+    "image": "https://www.calend.ru/img/content/i2/2850.jpg",
+    "title": "Oltinchi yangilik",
+    "body": "Dasturni testlab ko'rish uchun yana yangilik qo'shib ko'rildi",
+    "type": [
+      {
+        "tibbiyot_rivoji": true,
+        "tibbiy_talim": false,
+        "emlash": true
+      }
+    ],
+    "creator": "Hojiboy Raupov",
+    "date": "2022-10-06",
+    "time": "01:20:00"
+  }
+]
